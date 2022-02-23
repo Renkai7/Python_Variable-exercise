9a8048be445d92e554ce1d01ebe5ad524dfefcb0
Michael Spencer <ms53150@gmail.com>
2022-02-23 01:08:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+# variable(X) holds the old value of variable(A)
+x = a
+a = b
+b = x
+
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
